06568a8fe8e2cdbbd2d23c80623ccd4ee1a9b8c8
Zipdemonharshits012 <64593254+Zipdemonharshits012@users.noreply.github.com>
2021-02-25 07:15:50 +0530

    Create DFS_Adjacency_List.cpp

---
+++ DFS_Adjacency_List.cpp
@@ -0,0 +1,61 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+public:
+    void dfs(int V, vector<int> adj[], int s, vector<int> &traversal, vector<bool> &visited)
+    {
+        if (visited[s])
+            return;
+        visited[s] = true;
+        traversal.push_back(s);
+        for (auto u : adj[s])
+        {
+            if (visited[u] == false)
+                dfs(V, adj, u, traversal, visited);
+        }
+    }
+    vector<int> dfsOfGraph(int V, vector<int> adj[])
+    {
+        vector<bool> visited(V, 0);
+        vector<int> traversal;
+        dfs(V, adj, 0, traversal, visited);
+
+        return traversal;
+    }
+};
+
+// { Driver Code Starts.
+int main()
+{
+    int tc;
+    cin >> tc;
+    while (tc--)
+    {
+        int V, E;
+        cin >> V >> E;
+
+        vector<int> adj[V];
+
+        for (int i = 0; i < E; i++)
+        {
+            int u, v;
+            cin >> u >> v;
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+        // string s1;
+        // cin>>s1;
+        Solution obj;
+        vector<int> ans = obj.dfsOfGraph(V, adj);
+        for (int i = 0; i < ans.size(); i++)
+        {
+            cout << ans[i] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+} // } Driver Code Ends
